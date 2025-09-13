3824d5aa7878ae55e2d206c723d02128cb12d8ec
Shashwat-Trivedi <shashwatrivedi2005@gmail.com>
2025-09-13 21:19:59 +0530

    basic logo

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
-import { logo, sun } from '../assets';
-import { navlinks } from '../constants';
-import { useStateContext } from '../context';
+import { logo, sun } from "../assets";
+import { navlinks } from "../constants";
+import { useStateContext } from "../context";
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
+  <div
+    className={`w-[48px] h-[48px] rounded-[10px] ${
+      isActive && isActive === name && "bg-[#2c2f32]"
+    } flex justify-center items-center ${
+      !disabled && "cursor-pointer"
+    } ${styles}`}
+    onClick={handleClick}
+  >
     {!isActive ? (
       <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
     ) : (
-      <img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
+      <img
+        src={imgUrl}
+        alt="fund_logo"
+        className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+      />
     )}
   </div>
-)
+);
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -21,8 +32,8 @@ const Sidebar = () => {
   const { disconnectWallet } = useStateContext();
 
   // Find the active link based on current pathname
-  const activeLink = navlinks.find(link => link.link === location.pathname);
-  const isActive = activeLink ? activeLink.name : '';
+  const activeLink = navlinks.find((link) => link.link === location.pathname);
+  const isActive = activeLink ? activeLink.name : "";
 
   const handleLogout = () => {
     disconnectWallet();
@@ -32,16 +43,18 @@ const Sidebar = () => {
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
       <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+        <div className="w-[52px] h-[52px] rounded-[10px] bg-[#1dc071] flex justify-center items-center cursor-pointer">
+          <img src={logo} alt="HeartChain Logo" width={56} height={56} />
+        </div>
       </Link>
 
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
         <div className="flex flex-col justify-center items-center gap-3">
-          {navlinks.map((link) => (
-            link.name === 'logout' ? (
+          {navlinks.map((link) =>
+            link.name === "logout" ? (
               <Icon
                 key={link.name}
-                styles={isActive === link.name ? 'bg-[#2c2f32]' : ''}
+                styles={isActive === link.name ? "bg-[#2c2f32]" : ""}
                 name={link.name}
                 imgUrl={link.imgUrl}
                 isActive={isActive}
@@ -49,24 +62,24 @@ const Sidebar = () => {
                 handleClick={handleLogout}
               />
             ) : (
-              <Icon 
+              <Icon
                 key={link.name}
                 {...link}
                 isActive={isActive}
                 handleClick={() => {
-                  if(!link.disabled) {
+                  if (!link.disabled) {
                     navigate(link.link);
                   }
                 }}
               />
             )
-          ))}
+          )}
         </div>
 
         {/* <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} /> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
